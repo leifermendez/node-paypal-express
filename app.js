e33678acfe9673383de2248fff9ac5ca295d0946
Leifer Mendez <leifer33@gmail.com>
2021-03-28 14:17:16 +0200

    Se agrego la subscripcion

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const request = require('request');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 /**
@@ -68,6 +69,115 @@ const executePayment = (req, res) => {
         res.json({ data: response.body })
     })
 }
+/**
+ * ⚡⚡⚡⚡⚡⚡  1️⃣
+ * @param {*} req 
+ * @param {*} res 
+ */
+const createProduct = (req, res) => {
+    const product = {
+        name: 'Subscripcion Youtube',
+        description: "Subscripcion a un canal de Youtube se cobra mensualmente",
+        type: 'SERVICE',
+        category: 'SOFTWARE',
+        image_url: 'https://avatars.githubusercontent.com/u/15802366?s=460&u=ac6cc646599f2ed6c4699a74b15192a29177f85a&v=4'
+
+    }
+
+    //https://developer.paypal.com/docs/api/catalog-products/v1/#products_create
+    request.post(`${PAYPAL_API}/v1/catalogs/products`, {
+        auth,
+        body: product,
+        json: true
+    }, (err, response) => {
+        res.json({ data: response.body })
+    })
+}
+
+/**
+ * ⚡⚡⚡⚡⚡⚡  2️⃣
+ * @param {*} req 
+ * @param {*} res 
+ */
+const createPlan = (req, res) => {
+    const { body } = req
+    //product_id
+
+    const plan = {
+        name: 'PLAN mensual',
+        product_id: body.product_id,
+        status: "ACTIVE",
+        billing_cycles: [
+            {
+                frequency: {
+                    interval_unit: "MONTH",
+                    interval_count: 1
+                },
+                tenure_type: "REGULAR",
+                sequence: 1,
+                total_cycles: 12,
+                pricing_scheme: {
+                    fixed_price: {
+                        value: "3", // PRECIO MENSUAL QUE COBRAS
+                        currency_code: "USD"
+                    }
+                }
+            }],
+        payment_preferences: {
+            auto_bill_outstanding: true,
+            setup_fee: {
+                value: "10",
+                currency_code: "USD"
+            },
+            setup_fee_failure_action: "CONTINUE",
+            payment_failure_threshold: 3
+        },
+        taxes: {
+            percentage: "10", // 10USD + 10% = 11 USD
+            inclusive: false
+        }
+    }
+
+    request.post(`${PAYPAL_API}/v1/billing/plans`, {
+        auth,
+        body: plan,
+        json: true
+    }, (err, response) => {
+        res.json({ data: response.body })
+    })
+}
+
+/**
+ * ⚡⚡⚡⚡⚡⚡  3️⃣
+ * @param {*} req 
+ * @param {*} res 
+ */
+const generateSubscription = (req, res) => {
+    const { body } = req
+
+    const subscription = {
+        plan_id: body.plan_id, //P-3HK92642FR4448515MBQHCYQ
+        start_time: "2021-11-01T00:00:00Z",
+        quantity: 1,
+        subscriber: {
+            name: {
+                given_name: "Leifer",
+                surname: "Mendez"
+            },
+            email_address: "customer@example.com",
+        },
+        return_url: 'http://localhost/gracias',
+        cancel_url: 'http://localhost/fallo'
+
+    }
+    request.post(`${PAYPAL_API}/v1/billing/subscriptions`, {
+        auth,
+        body: subscription,
+        json: true
+    }, (err, response) => {
+        res.json({ data: response.body })
+    })
+}
 
 /**
  * 2️⃣ Creamos Ruta para generar pagina de CHECKOUT
@@ -83,6 +193,26 @@ app.post(`/create-payment`, createPayment)
 
 app.get(`/execute-payment`, executePayment)
 
+
+//--------------------------------- SUBSCRIPCIONES --------------------------------------
+
+/**
+ * ⚡ Crear producto en PAYPAL
+ */
+
+app.post(`/create-product`, createProduct)
+
+/**
+ * ⚡ Crear plan en PAYPAL
+ */
+
+app.post(`/create-plan`, createPlan)
+
+/**
+ * ⚡ Crear subscripcion en PAYPAL
+ */
+
+app.post(`/generate-subscription`, generateSubscription)
 
 
 app.listen(3000, () => {
